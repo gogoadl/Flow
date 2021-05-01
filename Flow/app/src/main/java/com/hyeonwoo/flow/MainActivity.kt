a0933044b00f51383cd49375759ac63f040d7a55
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2021-05-01 18:43:31 +0900

    Navigation 의존성 추가 및 Fragment 생성

--- Flow/app/src/main/java/com/hyeonwoo/flow/MainActivity.kt
+++ Flow/app/src/main/java/com/hyeonwoo/flow/MainActivity.kt
@@ -1,26 +1,17 @@
 package com.hyeonwoo.flow
 
-import android.media.AudioManager
-import android.media.MediaActionSound
-import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.SeekBar
-import android.widget.TextView
 import com.google.android.exoplayer2.SimpleExoPlayer
-import org.w3c.dom.Text
-import java.lang.Exception
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-
+    /*
+    * 요구사항 정리하기
+    * 1. 음악 재생은 Foreground Service 또는 Worker 이용이 필요 해 보임. -> Background 이동 후 상태 바에서 슬라이드로 삭제가 불가능하므로.
+    * 2. 음악 재생바 (Exoplayer), Fragment선택 바는 Activity에 고정하고, 위의 뷰는 navigation 이용하여 화면 전환하기
+    *
+    * */
     private var player : SimpleExoPlayer? = null
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
